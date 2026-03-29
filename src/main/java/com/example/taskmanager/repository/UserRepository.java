2dddb72bb6b157af69f813808f01d7659a6c5f31
Bhanu Anumula <bhanuprasadanumula@gmail.com>
2026-03-29 17:11:50 +0530

    fix(repository): add annotation

--- src/main/java/com/example/taskmanager/repository/UserRepository.java
+++ src/main/java/com/example/taskmanager/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
